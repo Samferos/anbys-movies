13f57ed319cf295e01e05fcf799991f0a02d9df9
jmentape <lohan.boeglin@etu.univ-nantes.fr>
2025-04-06 17:12:22 +0200

    Methode pour recuperer les films populaires, tous les genres et un film avec detail

--- app/src/main/java/iut/s4/sae/network/MovieDao.kt
+++ app/src/main/java/iut/s4/sae/network/MovieDao.kt
@@ -1,5 +1,13 @@
 package iut.s4.sae.network
 
+import android.util.Log
+import io.ktor.client.call.body
+import io.ktor.client.request.get
+import io.ktor.client.request.parameter
+import iut.s4.sae.model.Genres
+import iut.s4.sae.model.Movie
+import iut.s4.sae.model.Movies
+
 class MovieDao private constructor(){
     companion object{
         @Volatile
@@ -11,7 +19,35 @@ class MovieDao private constructor(){
             }
         }
     }
-
     private val BASE_URL = "https://api.themoviedb.org/3"
     private val API_KEY = ""
+    private val LANGUAGE = "fr"
+
+    suspend fun fetchGenres(): Genres {
+        val response: Genres = KtorClient.client.get("$BASE_URL/genre/movie/list") {
+            parameter("api_key", API_KEY)
+            parameter("language", LANGUAGE)
+        }.body()
+        return response
+    }
+
+    suspend fun fetchTrendingMovies(timeWindow: String = "day"): Movies {
+        val response: Movies = KtorClient.client.get("$BASE_URL/trending/movie/$timeWindow") {
+            parameter("api_key", API_KEY)
+            parameter("language", LANGUAGE)
+        }.body()
+        return response
+    }
+
+    suspend fun fetchMovieDetails(movieId: String): Movie {
+        val url = "$BASE_URL/movie/$movieId"
+        val response: Movie = KtorClient.client.get(url) {
+            parameter("api_key", API_KEY)
+            parameter("language", LANGUAGE)
+        }.body()
+        return response
+    }
+
+
+
 }
