c2f12e2b63a786471b1e8d14b5220fbaa5cdd84f
jmentape <lohan.boeglin@etu.univ-nantes.fr>
2025-04-06 15:35:35 +0200

    Fix de la class Genres

--- app/src/main/java/iut/s4/sae/model/Genres.kt
+++ app/src/main/java/iut/s4/sae/model/Genres.kt
@@ -4,11 +4,4 @@ import iut.s4.sae.network.MovieDao
 import kotlinx.serialization.Serializable
 
 @Serializable
-data class Genres(val genres: List<Genre>) {
-
-    companion object {
-        suspend fun init(): Genres {
-            return MovieDao.getInstance().fetchGenres()
-        }
-    }
-}
+data class Genres(val genres: List<Genre>)
