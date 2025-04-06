304941142830869ce485884d589a6f81fa6e9fbc
samuel <samuelamaralantunes@gmail.com>
2025-04-06 16:52:31 +0200

    stub(main): setting up main activity skeleton

--- app/src/main/java/iut/s4/sae/ui/MainActivity.kt
+++ app/src/main/java/iut/s4/sae/ui/MainActivity.kt
@@ -3,9 +3,12 @@ package iut.s4.sae.ui
 import android.content.Intent
 import android.os.Bundle
 import android.util.Log
+import android.view.Menu
+import android.view.MenuItem
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
+import androidx.core.view.get
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import iut.s4.sae.R
 
@@ -20,15 +23,42 @@ class MainActivity : AppCompatActivity() {
         val bottomBar = findViewById<BottomNavigationView>(R.id.main_bottom_navbar)
         bottomBar.setOnItemSelectedListener {
             when (it.itemId) {
+                R.id.bottom_bar_movies_menu -> {
+                    TODO("Display trending movies fragment")
+                }
+                R.id.bottom_bar_favourite_movies_menu -> {
+                    TODO("Display favourite movies fragment")
+                }
                 R.id.bottom_bar_settings_menu -> {
                     startActivity(Intent(this@MainActivity, SettingsActivity::class.java))
-                    bottomBar.selectedItemId = R.id.bottom_bar_movies_menu
-                    true
+                    false
                 }
                 else -> {
-                    true
+                    false
                 }
             }
         }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        val appBar = findViewById<Toolbar>(R.id.main_search_bar)
+        appBar.inflateMenu(R.menu.search_bar_menu)
+        appBar.setOnMenuItemClickListener {
+            when (it.itemId) {
+                R.id.search_bar_advanced_search_menu -> {
+                    TODO("Start advanced search activity")
+                }
+                R.id.search_bar_history_menu -> {
+                    TODO("Start history activity")
+                }
+                R.id.search_bar_clear_history_menu -> {
+                    TODO("Clear history")
+                }
+                else -> {
+                    false
+                }
+            }
+        }
+        return super.onCreateOptionsMenu(menu)
     }
 }
